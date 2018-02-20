4bade9c17a14962c6954456ea712635e38d4c479
Harrison Leath <hleath@me.com>
2018-02-20 11:49:49 -0500

    Initial Commit

---
+++ Login Screen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Login Screen
+//
+//  Created by Harrison Leath on 2/20/18.
+//  Copyright © 2018 Harrison Leath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Login ScreenTests/Login_ScreenTests.swift
@@ -0,0 +1,36 @@
+//
+//  Login_ScreenTests.swift
+//  Login ScreenTests
+//
+//  Created by Harrison Leath on 2/20/18.
+//  Copyright © 2018 Harrison Leath. All rights reserved.
+//
+
+import XCTest
+@testable import Login_Screen
+
+class Login_ScreenTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
